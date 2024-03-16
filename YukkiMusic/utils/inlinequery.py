0ab870ee0bddaca59ea5f96d6cd4ba7a659cfd2e
mafia132 <103453555+mafia132@users.noreply.github.com>
2024-03-16 20:41:14 +0300

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -1,12 +1,11 @@
 #
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-#
 
 from pyrogram.types import (InlineQueryResultArticle,
                             InputTextMessageContent)
@@ -16,56 +15,164 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="Pause Stream",
-            description=f"Pause the current playout on group call.",
+            title="ايقاف مؤقت",
+            description=f"لايقاف الاغنية بشكل مؤقت",
             thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
-            input_message_content=InputTextMessageContent("/pause"),
+            input_message_content=InputTextMessageContent("ايقاف مؤقت."),
         ),
         InlineQueryResultArticle(
-            title="Resume Stream",
-            description=f"Resume the ongoing playout on group call.",
+            title="استئناف التشغيل",
+            description=f"لاستئناف التشغيل بعد الايقاف المؤقت",
             thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
-            input_message_content=InputTextMessageContent("/resume"),
+            input_message_content=InputTextMessageContent(".استئناف"),
         ),
         InlineQueryResultArticle(
-            title="Mute Stream",
-            description=f"Mute the ongoing playout on group call.",
+            title="كتم الحساب المساعد",
+            description=f"لكتم الصوت في المكالمة",
             thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
-            input_message_content=InputTextMessageContent("/mute"),
+            input_message_content=InputTextMessageContent(".اسكت"),
         ),
         InlineQueryResultArticle(
-            title="Unmute Stream",
-            description=f"Unmute the ongoing playout on group call.",
+            title="إلغاء الكتم",
+            description=f"لالغاء كتم الحساب المساعد",
             thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
-            input_message_content=InputTextMessageContent("/unmute"),
+            input_message_content=InputTextMessageContent("الغاء الكتم."),
         ),
         InlineQueryResultArticle(
-            title="Skip Stream",
-            description=f"Skip to next track. | For Specific track number: /skip [number] ",
+            title="التخطي",
+            description=f"تخطي وتشغيل الانتظار تقدر تستخدم الامر كذا | /تخطي 3",
             thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
-            input_message_content=InputTextMessageContent("/skip"),
+            input_message_content=InputTextMessageContent("تخطي."),
         ),
         InlineQueryResultArticle(
-            title="End Stream",
-            description="Stop the ongoing playout on group call.",
+            title="ايقاف التشغيل",
+            description="لانها التشغيل",
             thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
-            input_message_content=InputTextMessageContent("/stop"),
+            input_message_content=InputTextMessageContent("ايقاف.#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram.types import (InlineQueryResultArticle,
+                            InputTextMessageContent)
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="ايقاف مؤقت",
+            description=f"لايقاف الاغنية بشكل مؤقت",
+            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            input_message_content=InputTextMessageContent("ايقاف مؤقت"),
         ),
         InlineQueryResultArticle(
-            title="Shuffle Stream",
-            description="Shuffle the queued tracks list.",
-            thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
-            input_message_content=InputTextMessageContent("/shuffle"),
+            title="استئناف التشغيل",
+            description=f"لاستئناف التشغيل بعد الايقاف المؤقت",
+            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            input_message_content=InputTextMessageContent("استئناف"),
         ),
         InlineQueryResultArticle(
-            title="Seek Stream",
-            description="Seek the ongoing stream to a specific duration.",
-            thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
-            input_message_content=InputTextMessageContent("/seek 10"),
+            title="كتم الحساب المساعد",
+            description=f"لكتم الصوت في المكالمة",
+            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
+            input_message_content=InputTextMessageContent("اسكت"),
         ),
         InlineQueryResultArticle(
-            title="Loop Stream",
-            description="Loop the current playing music. | Usage: /loop [enable|disable]",
+            title="إلغاء الكتم",
+            description=f"لالغاء كتم الحساب المساعد",
+            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
+            input_message_content=InputTextMessageContent("الغاء الكتم"),
+        ),
+        InlineQueryResultArticle(
+            title="التخطي",
+            description=f"تخطي وتشغيل الانتظار تقدر تستخدم الامر كذا | /تخطي 3",
+            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            input_message_content=InputTextMessageContent("تخطي"),
+        ),
+        InlineQueryResultArticle(
+            title="ايقاف التشغيل",
+            description="لانها التشغيل",
+            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            input_message_content=InputTextMessageContent("ايقاف"),
+        ),
+        InlineQueryResultArticle(
+            title="تكرار التشغيل",
+            description="لتكرار التشغيل مثال | /loop 4",
+            thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram.types import (InlineQueryResultArticle,
+                            InputTextMessageContent)
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="ايقاف مؤقت",
+            description=f"لايقاف الاغنية بشكل مؤقت",
+            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            input_message_content=InputTextMessageContent("ايقاف مؤقت"),
+        ),
+        InlineQueryResultArticle(
+            title="استئناف التشغيل",
+            description=f"لاستئناف التشغيل بعد الايقاف المؤقت",
+            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            input_message_content=InputTextMessageContent("استئناف"),
+        ),
+        InlineQueryResultArticle(
+            title="كتم الحساب المساعد",
+            description=f"لكتم الصوت في المكالمة",
+            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
+            input_message_content=InputTextMessageContent("اسكت"),
+        ),
+        InlineQueryResultArticle(
+            title="إلغاء الكتم",
+            description=f"لالغاء كتم الحساب المساعد",
+            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
+            input_message_content=InputTextMessageContent("الغاء الكتم"),
+        ),
+        InlineQueryResultArticle(
+            title="التخطي",
+            description=f"تخطي وتشغيل الانتظار تقدر تستخدم الامر كذا | /تخطي 3",
+            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            input_message_content=InputTextMessageContent("تخطي"),
+        ),
+        InlineQueryResultArticle(
+            title="ايقاف التشغيل",
+            description="لانها التشغيل",
+            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            input_message_content=InputTextMessageContent("ايقاف"),
+        ),
+        InlineQueryResultArticle(
+            title="تكرار التشغيل",
+            description="لتكرار التشغيل مثال | /loop 4",
+            thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
+"),
+        ),
+        InlineQueryResultArticle(
+            title="تكرار التشغيل",
+            description="لتكرار التشغيل مثال | /loop 4",
             thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
