ac48f4783a39ec50d431c69904a664564e71729e
mafia132 <103453555+mafia132@users.noreply.github.com>
2024-03-16 20:41:54 +0300

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -1,14 +1,13 @@
 #
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-#
 
-from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
+from config import LOG, LOG_GROUP_ID
 from YukkiMusic import app
 from YukkiMusic.utils.database import is_on_off
 
@@ -20,17 +19,20 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**{MUSIC_BOT_NAME} Play Log**
+**- عزيزي المطور اليك مجموعة قامت بتفعيل البوت .**
+**ٴ━━━━━━━━━━━━━**
+**- معلومات المجموعة :**
+**⤈︙اسم المجموعة :** ‹ {message.chat.title} › .
+**⤈︙ايدي المجموعة :** ‹ [{message.chat.id}] › .
+**⤈︙ رابط المجموعة :** ‹ {chatusername} › .
+**ٴ━━━━━━━━━━━━━**
+**- معلومات المتفاعل :**
+**⤈︙ اسم المتفاعل :** ‹ {message.from_user.mention} › .
+**⤈︙معرف المتفاعل :** ‹ @{message.from_user.username} › .
+**⤈︙ ايدي المتفاعل :** ‹ {message.from_user.id} › .
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {message.from_user.mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
-
-**Query:** {message.text}
-
-**StreamType:** {streamtype}"""
+**عنوان التشغيل:** ‹ {message.text} › .
+**- نوع التشغيل : ‹ {streamtype} › .**"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
